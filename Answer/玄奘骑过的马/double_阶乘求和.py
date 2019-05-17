c8c359d905aaef933e8d3107e4d8f47eb542343d
Oldyuan <wudy@riswein.com>
2019-05-17 12:46:22 +0800

    阶乘

--- Answer/玄奘骑过的马/double_阶乘求和.py
+++ Answer/玄奘骑过的马/double_阶乘求和.py
@@ -3,6 +3,20 @@
 
 
 
+def factorialAdd():
+    sums = 1
+    for i in range(2,20):
+        sum1 = i *(i-1)
+        print(sum1)
+        sums = sums + sum1
+    print(sums+1)
+
+
+
+if __name__ =="__main__":
+    factorialAdd()
+
+
 
 
 
