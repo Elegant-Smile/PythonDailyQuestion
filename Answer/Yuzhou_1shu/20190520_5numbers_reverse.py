f14eff0c5ce6408960d1f900540c2b66b90b5de5
yuzhou_1su <yuzhou_1shu@163.com>
2019-05-20 21:29:26 +0800

    增加判断

---
+++ Answer/Yuzhou_1shu/20190520_5numbers_reverse.py
@@ -0,0 +1,6 @@
+number = input("请输入一个不多于5位的正整数：")
+if len(number) > 5 or int(number) < 0:
+    print("Error, 请输入一个不多于5位的正整数：")
+else:
+    print("输入正整数的长度为：", len(number))
+    print("逆序打印出各位数字", number[::-1])
