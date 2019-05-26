9b462bebfa78e0804ce22883f585790b2da012d8
SkYe <liangliheng231@gmail.com>
2019-05-26 21:41:32 +0800

    fix error

--- Answer/SkYe/20190522_约瑟夫环问题.py
+++ Answer/SkYe/20190522_约瑟夫环问题.py
@@ -1,15 +1,20 @@
 n = int(input("输入参与人数："))
-steps, num_list = 3, list(map(int, list(range(1, n + 1))))
+steps, num_list,kill_num_list = 3, list(map(int, list(range(1, n + 1)))), []
 step, num_list_bk = steps - 1, num_list[:]
 
 
 def ysfh(steps):
     global num_list, step
-    out_number = num_list.pop(step)
+    out_number = num_list[step]
+    kill_num_list.append(out_number)
     print("{} was killed!".format(out_number))
+    print(num_list)
+    print(step)
     step = step + steps
     while step >= len(num_list):
         step -= len(num_list)
+        num_list = list(set(num_list)-set(kill_num_list))
+        num_list.sort()
     else:
         if len(num_list) == 1:
             pass
@@ -18,4 +23,4 @@ def ysfh(steps):
 
 
 ysfh(steps)
-print("最后一个存活的是{}号".format(num_list_bk.index(num_list.pop(0))))
+print("最后一个存活的是{}号".format(num_list_bk.index(num_list.pop(0))+1))
--- Answer/SkYe/20190523_分数和.py
+++ Answer/SkYe/20190523_分数和.py
@@ -2,10 +2,10 @@ num = int(input("输入一个整数："))
 if num%2 == 0:
     out_print = 0
     for i in range(2,num+1,2):
-        out_print += 1/num
+        out_print += 1/i
     print(out_print)
 else:
     out_print = 0
     for i in range(1,num+1,2):
-        out_print += 1/num
+        out_print += 1/i
         print(out_print)
