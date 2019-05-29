a633e4ec984032ba8f3b7362139a4c7c6ce1609a
yuzhou_1su <yuzhou_1shu@163.com>
2019-05-29 22:21:36 +0800

    修改528基础题

--- Answer/Yuzhou_1shu/20190528_1_cal_distances.py
+++ Answer/Yuzhou_1shu/20190528_1_cal_distances.py
@@ -6,9 +6,5 @@ def cal_distance(x1, y1, x2, y2):
 
 
 if __name__ == '__main__':
-    x1, y1, x2, y2 = input("请输入坐标数字:").split()
-    x1 = float(x1)
-    y1 = float(y1)
-    x2 = float(x2)
-    y2 = float(y2)
+    x1, y1, x2, y2 = map(float, input("请输入坐标数字:").split())
     print(cal_distance(x1, y1, x2, y2))
