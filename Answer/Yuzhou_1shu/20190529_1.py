122e07a75da91706cd918c33bf83c67417b7965c
yuzhou_1su <yuzhou_1shu@163.com>
2019-05-30 22:31:22 +0800

    完成529基础题

--- Answer/Yuzhou_1shu/20190529_1.py
+++ Answer/Yuzhou_1shu/20190529_1.py
@@ -1,6 +1,6 @@
 a = 809
 for i in range(10, 100):
     b = a * i + 1
-    if b >= 1000 and b <= 10000 and 8 * i < 100 and 9 * i >= 100:
+    if b >= 1000 and b <= 10000 and 8 * i < 100 and 9 * i > 99:
         print(i)
         print(b)
